fc07dac80d01a77a4a03567ec80a861875ca0f2d
Monika Gapinska <gapinska.mon@gmail.com>
2021-06-09 13:51:21 +0200

    useReducer added

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,31 +1,82 @@
-import { useState, useEffect, useCallback, useMemo } from "react"
+import { useState, useEffect, useCallback, useMemo, useReducer } from "react"
 import axios from "axios"
+
+const LOADING_ACTIVATED = "LOADING_ACTIVATED"
+const DATA_FETCH_SUCCESS = "DATA_FETCH_SUCCESS"
+const DATA_FETCH_FAILURE = "DATA_FETCH_FAILURE"
+const ADD_HIT = "ADD_HIT"
+const UPDATE_CHANGE = "UPDATE_CHANGE"
+const UPDATE_ON_SUBMIT = "UPDATE_ON_SUBMIT"
+
+const initialState = {
+  query: "",
+  data: "",
+  searchPhrase: "",
+  errors: null,
+  isLoading: false,
+}
+
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case LOADING_ACTIVATED:
+      return { ...state, isLoading: true }
+    case DATA_FETCH_SUCCESS:
+      return {
+        ...state,
+        data: action.payload,
+        searchPhrase: "",
+        isLoading: false,
+      }
+    case DATA_FETCH_FAILURE:
+      return { ...state, errors: action.payload, isLoading: false }
+    case ADD_HIT:
+      return { ...state, data: [...state.data, action.payload] }
+    case UPDATE_CHANGE:
+      return {
+        ...state,
+        query: action.payload,
+        data: null,
+      }
+    case UPDATE_ON_SUBMIT:
+      return {
+        ...state,
+        searchPhrase: state.query,
+      }
+    default:
+      return state
+  }
+}
+
 export const useAPI = () => {
-  const [state, setState] = useState({
-    query: "",
-    data: "",
-    searchPhrase: "",
-    errors: null,
-    isLoading: false,
-  })
+  const [state, dispatch] = useReducer(dataFetchReducer, initialState)
+  // const [state, setState] = useState({
+  //   query: "",
+  //   data: "",
+  //   searchPhrase: "",
+  //   errors: null,
+  //   isLoading: false,
+  // })
   const newSearch = useMemo(() => state.searchPhrase, [state.searchPhrase])
 
   const fetchData = useCallback(() => {
     if (!state.searchPhrase) return
-    setState({ ...state, isLoading: true })
+    dispatch({ type: LOADING_ACTIVATED })
+    // setState({ ...state, isLoading: true })
     axios
       .get(`https://hn.algolia.com/api/v1/search?query=${state.searchPhrase}`)
       .then((res) => {
         const result = res.data
-        setState({
-          ...state,
-          data: result.hits,
-          searchPhrase: "",
-          isLoading: false,
-        })
+        dispatch({ type: DATA_FETCH_SUCCESS, payload: result.hits })
+        // setState({
+        //   ...state,
+        //   data: result.hits,
+        //   searchPhrase: "",
+        //   isLoading: false,
+        // })
       })
       .catch((err) => {
-        setState({ ...state, errors: err.message, isLoading: false })
+        dispatch({ type: DATA_FETCH_FAILURE, payload: err.message })
+        // setState({ ...state, errors: err.message, isLoading: false })
       })
   }, [newSearch])
 
@@ -36,7 +87,8 @@ export const useAPI = () => {
   }
 
   const addHit = () => {
-    setState({ ...state, data: [...state.data, hit] })
+    dispatch({ type: ADD_HIT, payload: hit })
+    // setState({ ...state, data: [...state.data, hit] })
   }
 
   useEffect(() => {
@@ -44,11 +96,12 @@ export const useAPI = () => {
   }, [newSearch, fetchData])
 
   const updateChange = (value) => {
-    setState({ ...state, query: value, data: null })
+    dispatch({ type: UPDATE_CHANGE, payload: value })
   }
 
   const updateOnSubmit = () => {
-    setState({ ...state, searchPhrase: state.query })
+    dispatch({ type: UPDATE_ON_SUBMIT })
+    // setState({ ...state, searchPhrase: state.query })
   }
 
   return {
